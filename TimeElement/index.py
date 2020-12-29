b4bb427bca925a4063197e8397b2998a46c9a157
robin <robin.matter@outlook.com>
2020-12-29 18:15:14 +0100

    Integrate end point math and tester

--- TimeElement/index.py
+++ TimeElement/index.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
 @app.route("/")
 def welcome():
   return "Welcome to TimeElement"
+
+#@app.route("/math")
+#def math():
+#  return "calculator: "
+
+
+math = [
+  { "1 + 1": 1+1}
+]
+
+
+@app.route('/math')
+def get_math():
+  return  jsonify(math)
+
+
+@app.route('/math', methods=['POST'])
+def add_math():
+  math.append(request.get_json())
+  return '', 204
