0860f9c707a52a2b76c0e55e044d9509e7c4517f
robin <robin.matter@outlook.com>
2021-01-04 15:35:15 +0100

    Try to make an overall view under \main

--- TimeElement/index.py
+++ TimeElement/index.py
@@ -1,3 +1,4 @@
+import self as self
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
@@ -24,12 +25,12 @@ list_add = [
 
 
 @app.route('/calculator/plus')
-def get_additor():
+def get_addition():
   return  jsonify(list_add)
 
 
 @app.route('/calculator/plus', methods=['POST'])
-def add_additor():
+def add_addition():
   new_key = ''
 
   input = request.get_json()
@@ -58,16 +59,16 @@ def sub(x, y):
 
 list_sub = [
   {"result1": sub(8,3)},
-  {"result2": sub(6,5)}
+  {"result2": sub(6,20)}
 ]
 
 @app.route('/calculator/minus')
-def get_substrator():
+def get_substraction():
   return  jsonify(list_sub)
 
 
 @app.route('/calculator/minus', methods=['POST'])
-def add_substrator():
+def add_substraction():
   new_key = ''
 
   input = request.get_json()
@@ -90,3 +91,118 @@ def add_substrator():
 
 ########################################################################################################################
 #divide
+
+def div(x, y):
+  result = x / y
+  return result
+
+
+list_div = [
+  {"result1": div(9,3)},
+  {"result2": div(24,2)}
+]
+
+@app.route('/calculator/divide')
+def get_division():
+  return  jsonify(list_div)
+
+
+@app.route('/calculator/divide', methods=['POST'])
+def add_division():
+  new_key = ''
+
+  input = request.get_json()
+  assert len(input) == 1
+  for r in input:
+    new_key = r
+
+  found = False
+  for element in list_div:
+    if new_key in element.keys():
+      print("div", new_key)
+      if input[new_key] == 0:
+        raise Exception("You can't divide by 0")
+      else:
+        element[new_key] = div(element[new_key],input[new_key])
+        found = True
+
+  if found == False:
+    list_div.append(input)
+
+  return '', 204
+
+
+########################################################################################################################
+#multiply
+
+def mul(x, y):
+  result = x * y
+  return result
+
+
+list_mul = [
+  {"result1": mul(8,3)},
+  {"result2": mul(6,5)}
+]
+
+@app.route('/calculator/multiply')
+def get_multiplication():
+  return  jsonify(list_mul)
+
+
+@app.route('/calculator/multiply', methods=['POST'])
+def add_multiplication():
+  new_key = ''
+
+  input = request.get_json()
+  assert len(input) == 1
+  for r in input:
+    new_key = r
+
+  found = False
+  for element in list_mul:
+    if new_key in element.keys():
+      print("mul", new_key)
+      element[new_key] = mul(element[new_key],input[new_key])
+      found = True
+
+  if found == False:
+    list_mul.append(input)
+
+  return '', 204
+
+
+########################################################################################################################
+#main
+
+
+
+
+list_main = [
+  {"result1": add(2,3)},
+  {"result2": add(6,5)}
+]
+list_main = list_add + list_sub
+
+@app.route('/calculator/main')
+def get_main():
+  return  jsonify(list_main)
+
+
+def add_main():
+  new_key = ''
+
+  for r in list_add:
+    new_key = r
+
+  found = False
+  for element in list_sub:
+    if new_key in element.keys():
+      print("main", new_key)
+      element[new_key] = add(element[new_key],list_add[new_key])
+      found = True
+
+  if found == False:
+    list_mul.append(list_add)
+
+  return '', 204
