427dbe9456405a74bf44a53c01fb9a226e68a2f1
robin <robin.matter@outlook.com>
2021-01-04 11:28:14 +0100

    Add minus function

--- TimeElement/index.py
+++ TimeElement/index.py
@@ -8,24 +8,27 @@ def welcome():
   return "Welcome to TimeElement"
 
 
+########################################################################################################################
+#plus
 
 def add(x, y):
   result = x + y
   return result
 
 
-list = [
+
+list_add = [
   {"result1": add(2,3)},
   {"result2": add(6,5)}
 ]
 
 
-@app.route('/additor')
+@app.route('/calculator/plus')
 def get_additor():
-  return  jsonify(list)
+  return  jsonify(list_add)
 
 
-@app.route('/additor', methods=['POST'])
+@app.route('/calculator/plus', methods=['POST'])
 def add_additor():
   new_key = ''
 
@@ -35,13 +38,55 @@ def add_additor():
     new_key = r
 
   found = False
-  for element in list:
+  for element in list_add:
     if new_key in element.keys():
       print("Add", new_key)
       element[new_key] = add(element[new_key],input[new_key])
       found = True
 
   if found == False:
-    list.append(input)
+    list_add.append(input)
 
   return '', 204
+
+########################################################################################################################
+#minus
+def sub(x, y):
+  result = x - y
+  return result
+
+
+list_sub = [
+  {"result1": sub(8,3)},
+  {"result2": sub(6,5)}
+]
+
+@app.route('/calculator/minus')
+def get_substrator():
+  return  jsonify(list_sub)
+
+
+@app.route('/calculator/minus', methods=['POST'])
+def add_substrator():
+  new_key = ''
+
+  input = request.get_json()
+  assert len(input) == 1
+  for r in input:
+    new_key = r
+
+  found = False
+  for element in list_sub:
+    if new_key in element.keys():
+      print("sub", new_key)
+      element[new_key] = sub(element[new_key],input[new_key])
+      found = True
+
+  if found == False:
+    list_sub.append(input)
+
+  return '', 204
+
+
+########################################################################################################################
+#divide
--- TimeElement/tester.py
+++ TimeElement/tester.py
@@ -1,6 +1,6 @@
 
 import pytest
-from index import add
+from index import add, sub
 
 
 
@@ -11,3 +11,11 @@ def test_add():
     assert add(5, 3) == 8
     assert add(2, 11) == 13
     assert add(8, 3) == 11
+
+
+def test_sub():
+    assert sub(7, 3) == 4
+    assert sub(8, 1) == 7
+    assert sub(5, 3) == 2
+    assert sub(2, 11) == -9
+    assert sub(8, 8) == 0
