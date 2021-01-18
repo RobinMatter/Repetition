d6060f27c34df96225bbe33d1a0928cdb9d6d858
robin <robin.matter@outlook.com>
2021-01-18 09:16:44 +0100

    Change bootstrap.sh port to 5000

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 export FLASK_APP=index.py
 export FLASK_ENV=development
-export FLASK_RUN_PORT=3000
+export FLASK_RUN_PORT=5000
 source $(pipenv --venv)/bin/activate
 flask run -h 0.0.0.0 #127.0.0.1
